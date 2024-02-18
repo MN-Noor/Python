cc68a6c210a9783187fb623a78fb9725b344200a
MN-Noor <mn05052002@gmail.com>
2024-02-18 13:25:34 +0500

    problem  238

--- Product_of_Array_Except_Self.py
+++ Product_of_Array_Except_Self.py
@@ -1,4 +1,4 @@
-lass Solution(object):
+class Solution(object):
     def productExceptSelf(self, nums):
         size=len(nums)
         l=[1]*size
@@ -13,3 +13,19 @@ lass Solution(object):
             result[i]=right*l[i]
             right=right*nums[i]
         return result
+    #2nd Time
+
+class Solution(object):
+    def productExceptSelf(self, nums):
+        pos,pre=1,1
+        res=[1]*len(nums)
+        for i in range(len(nums)):
+            res[i]=pre
+            pre*=nums[i]
+        for i in range(len(nums)-1,-1,-1):
+            res[i]=res[i]*pos
+            pos*=nums[i]
+        return res
+
+        
+        
