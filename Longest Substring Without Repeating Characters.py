eb22052ca4a5c0621ffbb217c82296bb8cf45609
MN-Noor <mn05052002@gmail.com>
2024-02-29 12:09:51 +0500

    second tim

--- Longest Substring Without Repeating Characters.py
+++ Longest Substring Without Repeating Characters.py
@@ -13,4 +13,16 @@ class Solution(object):
                 l+=1
 
         return res
-
+# //second
+class Solution(object):
+    def lengthOfLongestSubstring(self, s):
+        l=0
+        check=set()
+        res=0
+        for r in range(len(s)):
+           while s[r] in check:
+               check.remove(s[l])
+               l+=1
+           check.add(s[r])
+           res=max(res,r-l+1)
+        return res
