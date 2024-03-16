90c4e1b6dc23803701f1d010f241176540b93915
MN-Noor <mn05052002@gmail.com>
2024-03-16 17:22:40 +0500

    2

--- DSA/linked_list/main.py
+++ DSA/linked_list/main.py
@@ -1,5 +1,5 @@
-import Node
-import LinkedList
+from linkedList import Node
+from linkedList import LinkedList
 def Menu():
     print("1:IsEmpty\n2:Insert At End\n3:Display\n4:insertA end\n5:count nodes\n6:display even values\n7:Sum of values\n8:Find\n9:Find Minimum \n10:Delete First node\n11:Destructer")
     choice=input("")
