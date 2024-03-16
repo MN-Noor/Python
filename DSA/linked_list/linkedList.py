3ab1710d42620d71f33df00d10aaf16c923e58a8
MN-Noor <mn05052002@gmail.com>
2024-03-16 17:21:24 +0500

    node functions added

---
+++ DSA/linked_list/linkedList.py
@@ -0,0 +1,92 @@
+class Node:
+    def __init__(self,data):
+        self.data=data
+        self.next=None
+    
+class LinkedList:
+    def __init__(self):
+        self.head=None
+
+    def IsEmpty(self):
+        if self.head==None:return True
+    def  insertAtStart(self,data):
+        new_node=Node(data)
+        if self.head==None:
+            self.head=new_node
+            return
+        else:
+            new_node.next=self.head
+            self.head=new_node
+    def insertAtIndex(self,data,pos):
+        cur_pos=1
+        new_node=Node(data)
+        curr_node=self.head
+        if cur_pos==pos:
+            self.head=new_node
+        else:
+            while curr_node.next!=None  and cur_pos+1==pos:
+                curr_node=curr_node.next
+                cur_pos+=1
+            new_node.next=curr_node
+            curr_node.next=new_node
+    def insertAtEnd(self,data):
+        node=Node(data)
+        if self.isEmpty():
+            self.head=node
+        else:
+            cur_node=self.head
+            while  cur_node.next!=None:
+                cur_node=cur_node.next()
+            cur_node.next=node
+    def countnodes(self):
+        count=0
+        if  self.isEmpty():
+            return 0
+        else:
+            while(current.next!=None):
+                current=self.head
+                current=current.next
+                count+=1
+            return count
+    def displayEven(self):
+        if self.IsEmpty():
+            print("Linked  list is Empty")
+        else:
+            current=self.head
+            while current.next!=None:
+                if current.data %2==0:
+                    print(current.data)
+                    current=current.next
+    def sumOfValues(self):
+        if self.isEmpty():
+            return 0
+        else:
+            sum=0
+            current=self.head
+            while current.next!=None:
+                sum+=current.data
+                current=current.next
+            return sum
+        
+    def find(self,value):
+        if self.IsEmpty():
+            return False
+        else:
+            current=self.head
+            while current.next!=None:
+                if current.data==value:
+                    return True
+                current=current.next
+            return False
+    def deleteNode():
+        
+    
+
+
+
+
+
+
+
+            
+            
--- DSA/linked_list/main.py
+++ DSA/linked_list/main.py
@@ -1,36 +1,10 @@
-class Node:
-    def __init__(self,data):
-        self.data=data
-        self.next=None
+import Node
+import LinkedList
+def Menu():
+    print("1:IsEmpty\n2:Insert At End\n3:Display\n4:insertA end\n5:count nodes\n6:display even values\n7:Sum of values\n8:Find\n9:Find Minimum \n10:Delete First node\n11:Destructer")
+    choice=input("")
 
-class LinkedList:
-    def __init__(self):
-        self.head=None
-    def  insertAtStart(self,data):
-        new_node=Node(data)
-        if self.head==None:
-            self.head=new_node
-            return
-        else:
-            new_node.next=self.head
-            self.head=new_node
-    def insertAtIndex(self,data,pos):
-        cur_pos=1
-        new_node=Node(data)
-        curr_node=self.head
-        if cur_pos==pos:
-            self.head=new_node
-        else:
-            while curr_node.next!=None  and cur_pos+1==pos:
-                curr_node=curr_node.next
-                cur_pos+=1
-            new_node.next=curr_node
-            curr_node.next=new_node
-
-            
-            
-            
-                
+             
             
 
         
